d701374d5b549054c38c5bbebec7b1c1a1132703
leantechlabs <info.leantechlabs@gmail.com>
2024-06-27 00:28:38 +0530

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+import { Mycontext } from "../Mycontext";
+import { useContext } from "react";
+export default function Login(){
+    return(
+        <div>
+
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Login from './components/Login'
+import TodoList from './components/TodoList';
+import Feedback from './components/Feedback';
+import './index.css'
+import {BrowserRouter as Router,
+  Routes,
+  Link,
+  Route} from 'react-router-dom'
+import Register from './components/Register';
+import Mycomponent from './Mycomponent';
+import Example from './Example';
+const root =document.getElementById('root')//root id from index.html
+const VirtualRoot = ReactDOM.createRoot(root);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+VirtualRoot.render(
+  <Router>
+  <div>  
+    <Header/>
+  </div>
+    <Feedback/>
+  <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+  </Router>
+)
